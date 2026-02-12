a582d5ccdd993a4a833abc1248b5c7982fef2cc7
DESBEEK Lucas <HE202497@students.ephec.be>
2026-02-12 15:24:31 +0100

    synch sequelize

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.listen(port, () => {
 function greet(name: string): string {
     return `Hello, ${name}!`;
 }
-let message = greet("Arnaud");
+let message = greet("Lucas");
 console.log(message);
 
 interface Etudiant {
@@ -28,9 +28,9 @@ interface Etudiant {
 }
 
 const etudiants: Etudiant[] = [
-    {id: 1, nom: "Dupont", prenom: "Jean"},
-    {id: 2, nom: "Martin", prenom: "Sophie"},
-    {id: 3, nom: "Doe", prenom: "John"}
+    {id: 1, nom: "François", prenom: "Jean"},
+    {id: 2, nom: "Colard", prenom: "Manon"},
+    {id: 3, nom: "Docteur", prenom: "Mike"}
 ];
 
 app.get('/api/data', (req: Request, res: Response) => {
@@ -53,3 +53,5 @@ try {
 } catch (error) {
   console.error('Unable to connect to the database:', error);
 }
+
+sequelize.sync({ force: true });
